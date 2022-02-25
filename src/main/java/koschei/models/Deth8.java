cd67759f1823db465b23313d28e81e208bd3f889
VIKuzmin <11v.kuzmin@gmail.com>
2022-02-25 12:07:29 +0300

    release chain with different methods

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,16 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    public Deth8() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
